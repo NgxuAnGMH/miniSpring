4a26264f3c5cef9e30d2a69b46ae0ce53584d654
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-23 20:43:02 +0800

    注释：BeanFactory.java

--- src/main/java/com/minis/beans/BeanFactory.java
+++ src/main/java/com/minis/beans/BeanFactory.java
@@ -14,7 +14,10 @@ public interface BeanFactory {
 
 	// 所有新增改动如下
 	boolean containsBean(String name);	// 是否有这个Bean
+
 	// void registerBean(String beanName, Object obj);
+	// 注册方法被分配给了【仓库接口】BeanDefinitionRegistry.java
+
 	boolean isSingleton(String name);	// 这个Bean是单例？
 	boolean isPrototype(String name);	// 这个Bean是原型？
 	Class<?> getType(String name);		// 获取Bean的类型。
