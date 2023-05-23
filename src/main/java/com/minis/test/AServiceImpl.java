ded4863637021d76e2abd960f94bfb7aef928a9c
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-23 15:46:46 +0800

    配置Setter注入：新增对应属性，然后对应生成getter和setter。
    顺便修改sayHello方法。

--- src/main/java/com/minis/test/AServiceImpl.java
+++ src/main/java/com/minis/test/AServiceImpl.java
@@ -1,12 +1,46 @@
 package com.minis.test;
 
 public class AServiceImpl implements AService {
+	// bean.xml文件的属性
 
-//	@Override
-	public void sayHello() {
-		// TODO Auto-generated method stub
-		System.out.println("a service 1 say hello.");
+	private String name;
+	private int level;
+	private String property1;
+	private String property2;
 
+	public String getProperty1() {
+		return property1;
 	}
+
+	public void setProperty1(String property1) {
+		this.property1 = property1;
+	}
+
+	public String getProperty2() {
+		return property2;
+	}
+
+	public void setProperty2(String property2) {
+		this.property2 = property2;
+	}
+
+//	public AServiceImpl() {
+//	}
+//
+//	public AServiceImpl(String name, int level) {
+//		this.name = name;
+//		this.level = level;
+//		System.out.println(this.name + "," + this.level);
+//	}
+
+	//	@Override
+	public void sayHello() {
+		System.out.println(this.property1 + "," + this.property2);
+	}
+//	IoC1的方式
+//	public void sayHello() {
+//		// TO-DO Auto-generated method stub
+//		System.out.println("a service 1 say hello.");
+//	}
 
 }
