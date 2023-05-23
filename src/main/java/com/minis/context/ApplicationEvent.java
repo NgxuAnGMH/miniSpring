ddd2e4050e4f79dd5cd335eea24b85c1f1c7fcea
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-23 22:03:16 +0800

    注释：ApplicationEvent.java & ApplicationEventPublisher.java
    事件监听与发布机制。预留扩展点

--- src/main/java/com/minis/context/ApplicationEvent.java
+++ src/main/java/com/minis/context/ApplicationEvent.java
@@ -2,6 +2,11 @@ package com.minis.context;
 
 import java.util.EventObject;
 
+/**
+ * 可以看出，ApplicationEvent 继承了 Java 工具包内的 *EventObject*，
+ * 我们是在 ==Java 的事件监听==的基础上进行了简单的封装。
+ * 虽然目前还没有任何实现，但这为我们后续使用==观察者模式==解耦代码提供了入口。
+ */
 public class ApplicationEvent extends EventObject {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/minis/context/ApplicationEventPublisher.java
+++ src/main/java/com/minis/context/ApplicationEventPublisher.java
@@ -1,5 +1,8 @@
 package com.minis.context;
 
+/**
+ * 预留的发布事件机制
+ */
 public interface ApplicationEventPublisher {
 	void publishEvent(ApplicationEvent event);
 }
