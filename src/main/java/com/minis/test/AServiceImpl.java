33dd5365d116e7be801b4639bbe18b2d4d46bd5e
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-23 19:35:11 +0800

    注释：beans.xml -> AServiceImpl.java

--- src/main/java/com/minis/test/AServiceImpl.java
+++ src/main/java/com/minis/test/AServiceImpl.java
@@ -1,42 +1,45 @@
 package com.minis.test;
 
 public class AServiceImpl implements AService {
-	// bean.xml文件的属性
+    // bean.xml中的Bean的属性
 
-	private String name;
-	private int level;
-	private String property1;
-	private String property2;
+    private String name;    // 构造器注入
+    private int level;        // 构造器注入
+    private String property1;    // Setter注入
+    private String property2;    // Setter注入
 
-	public String getProperty1() {
-		return property1;
-	}
+    // Setter注入
+    public String getProperty1() {
+        return property1;
+    }
 
-	public void setProperty1(String property1) {
-		this.property1 = property1;
-	}
+    public void setProperty1(String property1) {
+        this.property1 = property1;
+    }
 
-	public String getProperty2() {
-		return property2;
-	}
+    public String getProperty2() {
+        return property2;
+    }
 
-	public void setProperty2(String property2) {
-		this.property2 = property2;
-	}
+    public void setProperty2(String property2) {
+        this.property2 = property2;
+    }
 
-	public AServiceImpl() {
-	}
+    public AServiceImpl() {
+    }
 
-	public AServiceImpl(String name, int level) {
-		this.name = name;
-		this.level = level;
-		System.out.println(this.name + "," + this.level);
-	}
+    public AServiceImpl(String name, int level) {
+        // 构造器注入
+        this.name = name;
+        this.level = level;
+        System.out.println(this.name + "," + this.level);
+    }
 
-	//	@Override
-	public void sayHello() {
-		System.out.println(this.property1 + "," + this.property2);
-	}
+    //	@Override
+    public void sayHello() {
+        System.out.println(this.property1 + "," + this.property2);
+    }
+
 //	IoC1的方式
 //	public void sayHello() {
 //		// TO-DO Auto-generated method stub
