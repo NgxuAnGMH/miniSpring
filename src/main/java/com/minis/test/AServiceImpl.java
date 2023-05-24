5061a8e226859c63f66648bb91c945850646ceb6
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-24 18:49:36 +0800

    修改：AServiceImpl.java
    新增了依赖引用

--- src/main/java/com/minis/test/AServiceImpl.java
+++ src/main/java/com/minis/test/AServiceImpl.java
@@ -8,6 +8,11 @@ public class AServiceImpl implements AService {
     private String property1;    // Setter注入
     private String property2;    // Setter注入
 
+	// IoC3新增依赖引用
+	private BaseService ref1;
+	public BaseService getRef1() {return ref1;}
+	public void setRef1(BaseService bs) {this.ref1 = bs;}
+
     // Setter注入
     public String getProperty1() {
         return property1;
@@ -32,12 +37,13 @@ public class AServiceImpl implements AService {
         // 构造器注入
         this.name = name;
         this.level = level;
-        System.out.println(this.name + "," + this.level);
+        // System.out.println(this.name + "," + this.level); IoC3注释掉
     }
 
     //	@Override
     public void sayHello() {
         System.out.println(this.property1 + "," + this.property2);
+        ref1.sayHello();
     }
 
 //	IoC1的方式
