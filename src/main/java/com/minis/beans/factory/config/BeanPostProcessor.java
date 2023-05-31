91d4ed4ed20a388064678809463cb866c0ed586d
NgxuAnGMH <ngxuangmh@qq.com>
2023-05-31 15:08:08 +0800

    新增BeanPostProcessor接口

--- src/main/java/com/minis/beans/factory/config/BeanPostProcessor.java
+++ src/main/java/com/minis/beans/factory/config/BeanPostProcessor.java
@@ -3,8 +3,9 @@ package com.minis.beans.factory.config;
 import com.minis.beans.factory.BeansException;
 
 public interface BeanPostProcessor {
+	// Bean初始化之前
 	Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException;
-
+	// Bean初始化之后
 	Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException;
 
 }
