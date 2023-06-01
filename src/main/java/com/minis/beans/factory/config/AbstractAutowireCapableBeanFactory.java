8391c0ed78ec85fa2c3718b036b605463dc28d17
NgxuAnGMH <ngxuangmh@qq.com>
2023-06-01 16:20:59 +0800

    AbstractAutowireCapable改成抽象类BeanPostProcessor新增方法

--- src/main/java/com/minis/beans/factory/config/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/minis/beans/factory/config/AbstractAutowireCapableBeanFactory.java
@@ -11,18 +11,19 @@ import java.util.List;
  * 继承了AbstractBeanFactory，但没有重写任何一个方法。
  * 意思是：它的重点在于提供额外的Bean处理器增强！
  */
-public class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
-	private final List<AutowiredAnnotationBeanPostProcessor> beanPostProcessors = new ArrayList<AutowiredAnnotationBeanPostProcessor>();
+public abstract class AbstractAutowireCapableBeanFactory
+		extends AbstractBeanFactory implements AutowireCapableBeanFactory{
+	private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<BeanPostProcessor>();
 	// 用*一个列表 beanPostProcessors* 记录*所有的 Bean 处理器*，这样可以按照需求注册若干个不同用途的处理器，然后调用处理器。
 	
-	public void addBeanPostProcessor(AutowiredAnnotationBeanPostProcessor beanPostProcessor) {
+	public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor) {
 		this.beanPostProcessors.remove(beanPostProcessor);	// 先去掉
 		this.beanPostProcessors.add(beanPostProcessor);		// 后添加
 	}
 	public int getBeanPostProcessorCount() {
 		return this.beanPostProcessors.size();
 	}
-	public List<AutowiredAnnotationBeanPostProcessor> getBeanPostProcessors() {
+	public List<BeanPostProcessor> getBeanPostProcessors() {
 		return this.beanPostProcessors;
 	}
 
@@ -30,7 +31,7 @@ public class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
 			throws BeansException {
 
 		Object result = existingBean;
-		for (AutowiredAnnotationBeanPostProcessor beanProcessor : getBeanPostProcessors()) {
+		for (BeanPostProcessor beanProcessor : getBeanPostProcessors()) {
 			beanProcessor.setBeanFactory(this);
 			// **对每个 Bean 处理器**，调用方法 postProcess`Before`Initialization。
 			result = beanProcessor.postProcessBeforeInitialization(result, beanName);
--- src/main/java/com/minis/beans/factory/config/BeanPostProcessor.java
+++ src/main/java/com/minis/beans/factory/config/BeanPostProcessor.java
@@ -1,11 +1,13 @@
 package com.minis.beans.factory.config;
 
 import com.minis.beans.factory.exception.BeansException;
+import com.minis.beans.factory.support.BeanFactory;
 
 public interface BeanPostProcessor {
 	// Bean初始化之前
 	Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException;
 	// Bean初始化之后
 	Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException;
-
+	// 设置bf
+	void setBeanFactory(BeanFactory beanFactory);
 }
